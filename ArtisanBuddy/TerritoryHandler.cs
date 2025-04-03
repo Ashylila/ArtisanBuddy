b043bcc702eb1e0f169f970753d17c4d7e99c6bf
Ashylila <ashy.suhr@gmail.com>
2025-04-04 01:50:57 +0200

    Refactor territory change handling to support asynchronous crafting checks

--- ArtisanBuddy/TerritoryHandler.cs
+++ ArtisanBuddy/TerritoryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Lumina.Excel.Sheets;
 using ArtisanBuddy;
 using ArtisanBuddy.EzIpc;
@@ -15,17 +16,27 @@ public class TerritoryHandler
         this.configuration = plugin.Configuration;
         Svc.ClientState.TerritoryChanged += OnTerritoryChanged;
     }
-    private void OnTerritoryChanged(ushort newTerritoryId)
+    private async void OnTerritoryChanged(ushort newTerritoryId)
     {
         var territory = Svc.Data.GetExcelSheet<TerritoryType>().FirstOrDefault(x => x.RowId == newTerritoryId);
-        if(ShouldStartCrafting(territory))
+        try
         {
-            StartCrafting();
+            if (await ShouldStartCrafting(territory))
+            {
+                StartCrafting();
+            }
+        }
+        catch (Exception e)
+        {
+            Svc.Log.Error(e.Message, "Error in OnTerritoryChanged");
         }
     }
-    private bool ShouldStartCrafting(TerritoryType territory)
+        
+    
+    private async Task<bool> ShouldStartCrafting(TerritoryType territory)
     {
         //if (!GatherbuddyReborn_IPCSubscriber.IsEnabled) return false;
+        await Task.Delay(2000);
         if (configuration.ShouldCraftOnEnter && territory.PlaceName.Value.Name.ExtractText().Contains("private", StringComparison.OrdinalIgnoreCase) && !GatherbuddyReborn_IPCSubscriber.IsAutoGatherEnabled() && !GatherbuddyReborn_IPCSubscriber.IsAutoGatherWaiting())
         {
             return true;
